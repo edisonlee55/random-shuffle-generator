386dbdbcf85d2a9fe9e4b043bd961d47fd96a660
Edison Lee <edisonlee@edisonlee55.com>
2021-01-16 13:22:09 +0800

    Init

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) edisonlee55
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function shuffleArray() {
+    let arr = $("#input").val().split("\n");
+    shuffle(arr);
+    let tmp = "";
+    for (let i = 0; i < arr.length; i++) {
+        tmp += arr[i];
+        tmp += "\n";
+    }
+    $("#output").val(tmp);
+}
